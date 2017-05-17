3d08b9b4d4ddcbf17a9ce714ccb782bf728a6a7f
ReemSoliman <reem.soliman77@gmail.com>
2017-05-17 00:47:16 -0700

    some cosmatics

--- ui.R
+++ ui.R
@@ -13,20 +13,20 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("MaryLand Infants Mortality and Deathes 1919-2015"),
+  titlePanel("Maryland State Infants Mortality and Deaths 1919-2015"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
       selectInput("race", "Choose Race:", c("White" = "wt", "Black"  = "blk", "Both" = "both")),
-      selectInput("Case", "Choose Death or Mortality:", c("Death" = "dth", "Mortality" = "mor")),
-      submitButton(text = "Apply Changes", icon = NULL, width = NULL)
+      selectInput("Case", "Choose Deaths or Mortality:", c("Deaths" = "dth", "Mortality" = "mor")),
+      submitButton(text = "Submit", icon = NULL, width = NULL)
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
     tabsetPanel( type = "tabs",
-       tabPanel ("Plot",  plotOutput("plot", height = "400px", width = "600px")),
+       tabPanel ("Historical Data",  plotOutput("plot", height = "400px", width = "600px")),
        tabPanel ("Predication",  "Prediction for the Years 2016 - 2020", br(), tableOutput("prediction") ,br(), plotOutput("prdplot") )
        )       
     )
